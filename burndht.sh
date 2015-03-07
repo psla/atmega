87d34503e744e5fe4836fe5c0e9a9502a3fc23c3
Piotr Slatala <psla-github@sepio.pl>
2015-03-07 11:53:37 -0800

    compile and burn DHT reader

---
+++ burndht.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+avr-gcc -mmcu=atmega328p -Wall -Os dhtreader.c lib/dht11.c && avr-objcopy -j .text -j .data -O ihex a.out a.out.hex && gpio -g write 8 0 && /usr/local/bin/avrdude -p m328p -P /dev/spidev0.0 -c linuxspi -b 10000 -e -U flash:w:a.out.hex && gpio -g write 8 1
+
