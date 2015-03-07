ed7c8ae91d59cbff97439bd54dd80c548c217b3d
Piotr Slatala <psla-github@sepio.pl>
2015-03-06 22:40:16 -0800

    Blink two diodes at a time. Connected to PB0 and PB6

---
+++ blink1.c
@@ -0,0 +1,18 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int
+main (void)
+{
+    DDRB |= _BV(DDB0); 
+    DDRB |= _BV(DDB6); 
+    
+    while(1) 
+    {
+        PORTB ^= _BV(PB0);
+        PORTB ^= _BV(PB6);
+        _delay_ms(100);
+    }
+}
