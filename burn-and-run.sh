6ba642cd96b7ae07073029bf5f64e9f3cb34a7fc
Piotr Slatala <psla-github@sepio.pl>
2015-03-06 22:40:04 -0800

    Burn and run routine

--- burn-and-run.sh
+++ burn-and-run.sh
@@ -11,3 +11,9 @@ then
   echo "file $1 does not exist"
   exit
 fi
+
+avr-gcc -mmcu=atmega328p -Wall -Os -o $1.elf $1
+avr-objcopy -j .text -j .data -O ihex $1.elf $1.hex
+gpio -g write 8 0
+/usr/local/bin/avrdude -p m328p -P /dev/spidev0.0 -c linuxspi -b 10000 -e -U flash:w:$1.hex
+gpio -g write 8 1
