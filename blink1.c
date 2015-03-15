fc3b559a0e8af24037cf4c18ad60f025304075c6
Piotr Slatala <psla-github@sepio.pl>
2015-03-15 15:55:05 -0700

    Add one more diode to blink

--- blink1.c
+++ blink1.c
@@ -8,11 +8,13 @@ main (void)
 {
     DDRB |= _BV(DDB0); 
     DDRB |= _BV(DDB6); 
+    DDRB |= _BV(DDB7); 
     
     while(1) 
     {
         PORTB ^= _BV(PB0);
         PORTB ^= _BV(PB6);
+        PORTB ^= _BV(PB7);
         _delay_ms(500);
     }
 }
