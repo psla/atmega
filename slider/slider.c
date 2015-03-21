03740338e13251e50d7e535ca0082855bc181343
Piotr Slatala <psla-github@sepio.pl>
2015-03-20 21:07:37 -0700

    Basic initialization of slider

--- slider/slider.c
+++ slider/slider.c
@@ -2,20 +2,31 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
- 
+#include "../lib/common.h"
+
+// TODO: Figure story for unit tests. cppUnit? uCNit? 
+
 #define BUTTON_PIN PB0
 #define LED_PIN PB1
 #define LED_DDB DDB1
+
+/// Switch on the left and right. High indicates that slider is on this side.
+/// This means, that platform is in the middle if both are "LOW"
+/// (this way we can preserve energy - most of the time slider will be
+/// driving from left to right, so having current flowing (even small current) is excessive)
+#define LEFT_SWITCH PB2
+#define RIGHT_SWITCH PB3
 
 // Fill this in. This is number of motor rotations per entire slide from side to side
 // This way we will know how many moves per picture to take
 // To determine this, run "motor test" and it will return (on screen) a number to put in here
 // TODO: persist data on arduino eprom
 #define ROTATIONS_PER_SLIDER 1000
-
+#define DIRECTION_RIGHT 1
+#define DIRECTION_LEFT 0
 
 typedef struct slider_state_t {
-  /// direction is +1 when slider is moving right
+  /// direction is 1 when slider is moving right
   /// and 0, when slider is moving left
   /// it should always reflect a desired direction of the next step
   /// (as opposed to the direction of the last step that took place)
@@ -42,16 +53,40 @@ typedef struct slider_state_t {
   uint8_t remaining_steps;
 } slider_state_t;
 
+slider_state_t slider_state;
 
 /// Drive as long as you can in direction
 /// until you reach (any) side
 void drive(uint8_t direction)
 {
     if(direction) {
-      /// TODO: move right
+      while(!IS_SET(PORTB, RIGHT_SWITCH))  {
+        // move right
+      }
     } else {
-      /// TODO: move left
+      while(!IS_SET(PORTB, LEFT_SWITCH)) {
+        // move left
+      }
     }
+}
+
+/// Verifies the platform position
+/// if platform is on the side, update a direction for the next sliding direction
+/// and return 1
+/// otherwise do nothing and return 0
+uint8_t update_direction_based_on_platform_position()
+{
+  if(IS_SET(PORTB, LEFT_SWITCH)) {
+    slider_state.direction = DIRECTION_RIGHT;
+    return 1;
+  }
+
+  if(IS_SET(PORTB, RIGHT_SWITCH)) {
+    slider_state.direction = DIRECTION_LEFT;
+    return 1;
+  }
+
+  return 0;
 }
 
 int
@@ -59,11 +94,16 @@ main (void)
 {
     DDRB |= _BV(LED_DDB);
 
-    
+    // see position of the platform and if not on the side, go to the side
+    // this operation is synchronous which means that it blocks UI / screen while the motor is moving
+    if(!update_direction_based_on_platform_position()) {
+      drive(DIRECTION_LEFT);
+      slider_state.direction = DIRECTION_RIGHT;
+    }
 
     while(1)
     {
-        PORTB ^= _BV(LED_PIN);
-        _delay_ms(500);
+      // we have just started go to programming mode
+
     }
 }
