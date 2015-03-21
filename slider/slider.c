99081e2820ea88b565977dfbc56d699a6a6ee6c1
Piotr Slatala <psla-github@sepio.pl>
2015-03-20 20:15:24 -0700

    Add basic struct for keeping slider status

--- slider/slider.c
+++ slider/slider.c
@@ -3,19 +3,53 @@
 #include <avr/io.h>
 #include <util/delay.h>
  
-struct 
+#define BUTTON_PIN PB0
+#define LED_PIN PB1
+#define LED_DDB DDB1
+
+// Fill this in. This is number of motor rotations per entire slide from side to side
+// This way we will know how many moves per picture to take
+// To determine this, run "motor test" and it will return (on screen) a number to put in here
+// TODO: persist data on arduino eprom
+#define ROTATIONS_PER_SLIDER 1000
+
+
+typedef struct slider_state_t {
+  /// direction is +1 when slider is moving right
+  /// and 0, when slider is moving left
+  /// it should always reflect a desired direction of the next step
+  /// (as opposed to the direction of the last step that took place)
+  /// this means that at the end of slide, you can switch it to opposite
+  uint8_t direction;
+
+  /// Number of rotations to make per step.
+  /// In other words, number of stepper motor steps to take between two pictures
+  /// This must be positive number
+  uint8_t speed;
+
+  /// Contains number of remaining pictures to take
+  /// This is also an estimate of remaining steps till the other end of the slider
+  /// (note that this might not be accurate, you might finish too early
+  /// or too late, depending on quality of the stepper motor)
+  /// Code and hardware will prevent 'too late' stops.
+  ///
+  /// Remaining steps is initially set to ROTATIONS_PER_SLIDER / speed
+  /// as long as speed is constant
+  /// TODO: in future we might want to consider variable speed of the motor
+  ///
+  /// This number can be used to trigger buzzer (when it reaches zero)
+  /// But you can continue going forward and taking pictures until you reach the other end of the slider
+  uint8_t remaining_steps;
+} slider_state_t;
+
 int
 main (void)
 {
-    DDRB |= _BV(DDB0);
-    DDRB |= _BV(DDB6);
-    DDRB |= _BV(DDB7);
-    
-    while(1) 
-    
-        PORTB ^= _BV(PB0);
-        PORTB ^= _BV(PB6);
-        PORTB ^= _BV(PB7);
+    DDRB |= _BV(LED_DDB);
+
+    while(1)
+    {
+        PORTB ^= _BV(LED_PIN);
         _delay_ms(500);
     }
 }
