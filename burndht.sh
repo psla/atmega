56a257e1a6ab3ad93f14fbd148695952e9604efb
Piotr Slatala <piotr@sepio.pl>
2015-03-28 16:01:41 -0700

    Update build files for DHT modules to include CPU FREQ

--- burndht.sh
+++ burndht.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-avr-gcc -mmcu=atmega328p -Wall -Os dhtreader.c lib/dht.c lib/blinker.c
+avr-gcc -DF_CPU=8000000L -mmcu=atmega328p -Wall -Os dhtreader.c lib/dht.c lib/blinker.c
 avr-objcopy -j .text -j .data -O ihex a.out a.out.hex
 gpio -g write 8 0
 /usr/local/bin/avrdude -p m328p -P /dev/spidev0.0 -c linuxspi -b 10000 -e -U flash:w:a.out.hex 
--- burndht22.sh
+++ burndht22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-avr-gcc -mmcu=atmega328p -Wall -Os dht22reader.c lib/dht.c lib/blinker.c
+avr-gcc -DF_CPU=8000000L -mmcu=atmega328p -Wall -Os dht22reader.c lib/dht.c lib/blinker.c
 avr-objcopy -j .text -j .data -O ihex a.out a.out.hex
 gpio -g write 8 0
 /usr/local/bin/avrdude -p m328p -P /dev/spidev0.0 -c linuxspi -b 40000 -e -U flash:w:a.out.hex && gpio -g write 8 1
