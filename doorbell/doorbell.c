9e65080f699b7af347becb75bd367b7da09eb693
Piotr (Peter) Slatala <psla-github@sepio.pl>
2017-03-04 12:03:40 -0800

    Doorbell & rfm2017, but it doesn't work (needs ino build)

--- doorbell/doorbell.c
+++ doorbell/doorbell.c
@@ -10,11 +10,37 @@
 #include <avr/interrupt.h>
 
 #include "../lib/common.h"
+#include "../rfm2017/RFM69.h"
 
 #define BUTTON_REGISTRY DDRD // is it input or output
 #define BUTTON_PIN		PIND // reading input
 #define BUTTON_PIN_INDEX	PD3 // index of the pin
 #define BUTTON_PORT		PORTD // voltage levels for pins (pullup)
+
+#define NODEID      4
+#define NETWORKID   100
+#define GATEWAYID   1
+#define FREQUENCY   RF69_915MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
+// TODO - deifne encryption
+#define KEY         0 //has to be same 16 characters/bytes on all nodes, not more not less!
+
+typedef struct Payload {
+	uint8_t  nodeId; //store this nodeId
+} Payload;
+
+
+RFM69 radio(RF69_SPI_CS, RF69_IRQ_PIN, true, RF69_IRQ_NUM);
+
+Payload dataToBeSent;
+
+void blink_repeat(uint8_t n) {
+	for(uint8_t i = 0; i < n; i++) {
+		SET_BIT(PORTB, PB0);
+		_delay_ms(350);
+		CLEAR_BIT(PORTB, PB0);
+		_delay_ms(150);
+	}
+}
 
 void sleep() {
 	// disable brown-out detection
@@ -32,6 +58,20 @@ ISR (INT0_vect)
 {
 	// wakeup. Clear interrupts, and wait for another sleep.
 	cli();
+}
+
+void ring_bell() {
+	dataToBeSent.nodeId = NODEID;
+
+	if (radio.sendWithRetry(GATEWAYID, (const void*)(&dataToBeSent), sizeof(dataToBeSent)))
+	{
+		// for testing you may uncomment line below
+		// blink_repeat(1);
+	}
+	else
+	{
+		blink_repeat(8);
+	}
 }
 
 int
@@ -59,14 +99,23 @@ main (void)
 	const uint8_t blinks_before_sleep = 20;
 	uint8_t blinks = 0;
 
+	// enable diode during initialization
+	SET_BIT(PORTB, PB0);
+	
+	radio.initialize(FREQUENCY,NODEID,NETWORKID);
+	radio.setHighPower();
+	radio.setPowerLevel(25);
+	radio.encrypt(KEY);
+
+
     while(1) 
     {
 		if(IS_SET(BUTTON_PIN, BUTTON_PIN_INDEX) == 1)
 		{
-			SET_BIT(PORTB, PB0);
+			CLEAR_BIT(PORTB, PB0);
 		} 
 		else {
-			CLEAR_BIT(PORTB, PB0);
+			SET_BIT(PORTB, PB0);
 			if(blinks > 2) {
 				blinks = 0;
 				counter = 0;
